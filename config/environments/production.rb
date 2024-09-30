d151b06e6610cd49fd611faa5890f2582ed2dbfa
Jamie Alessio <jamie@stoic.net>
2024-09-30 11:04:01 -0700

    this seems hacky but maybe it unblocks me

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files on Cloudflare R2
-  config.active_storage.service = :cloudflare
+  config.active_storage.service = ENV["SECRET_KEY_BASE_DUMMY"].present? ? :local : :cloudflare
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
